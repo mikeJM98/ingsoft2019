9c557b4463adce0343eb7c29a9f4981a9541055c
mikeJM98 <miquiaset91@gmail.com>
2019-05-19 11:30:38 -0500

    entrgable2

--- views/layout/menu.php
+++ views/layout/menu.php
@@ -32,10 +32,30 @@
 							</span>
 						</a>
 						<ul class="treeview-menu" id="ESTADIAS">
-							<li><a href="entrada.php"><i class="fa fa-circle-o"></i> Entradas</a></li>
-							<li><a href="reserva.php"><i class="fa fa-circle-o"></i> Reservas</a></li>
-							<li><a href="huesped.php"><i class="fa fa-circle-o"></i> Cliente</a></li>
-							<!--<li><a href="#cliente.php"><i class="fa fa-circle-o"></i> Cliente</a></li>-->
+							<li><a href="huesped.php"><i class="fa fa-check-circle-o"></i> Huesped</a></li>
+							<li><a href="entrada.php"><i class="fa fa-check-circle-o"></i> Entradas</a></li>
+							<li><a href="reserva.php"><i class="fa fa-check-circle-o"></i> Reservas</a></li>
+							<!--<li><a href="#cliente.php"><i class="fa fa-check-circle-o"></i> Cliente</a></li>-->
+						</ul>
+					</li>
+				<?php } ?>
+				<?php
+				if (in_array('clientes', $_SESSION['modulos'])) { ?>
+					<li class="treeview ESTADIAS">
+						<a href="#">
+							<i class="fa fa-users"></i>CLIENTES <span></span>
+							<span class="pull-right-container">
+								<i class="fa fa-angle-left pull-right"></i>
+							</span>
+						</a>
+						<ul class="treeview-menu" id="ESTADIAS">
+							<li><a href="cliente.php"><i class="fa fa-check-circle-o"></i> Cliente</a></li>
+							<li><a href="tipo_cliente.php"><i class="fa fa-check-circle-o"></i> Tipo cliente</a></li>
+							<li><a href="tipo_documento.php"><i class="fa fa-check-circle-o"></i> Tipo documento</a></li>
+							<li><a href="pais.php"><i class="fa fa-check-circle-o"></i> Pais</a></li>
+							<li><a href="ciudad.php"><i class="fa fa-check-circle-o"></i> Ciudad</a></li>
+
+							<!--<li><a href="#cliente.php"><i class="fa fa-check-circle-o"></i> Cliente</a></li>-->
 						</ul>
 					</li>
 				<?php } ?>
@@ -49,8 +69,8 @@
 							</span>
 						</a>
 						<ul class="treeview-menu" id="HABITACION">
-							<li><a href="habitacion.php"><i class="fa fa-circle-o"></i> Habitacion</a></li>
-							<li><a href="tipo_habitacion.php"><i class="fa fa-circle-o"></i> Tipo habitacion</a></li>
+							<li><a href="habitacion.php"><i class="fa fa-check-circle-o"></i> Habitacion</a></li>
+							<li><a href="tipo_habitacion.php"><i class="fa fa-check-circle-o"></i> Tipo habitacion</a></li>
 						</ul>
 					</li>
 				<?php } ?>
@@ -58,14 +78,14 @@
 				if (in_array('reportes', $_SESSION['modulos'])) { ?>
 					<li class="treeview">
 						<a href="#">
-							<i class="fa fa-print"></i> <span>REPORTES</span>
+							<i class="fa fa-tachometer"></i> <span>REPORTES</span>
 							<span class="pull-right-container">
 								<i class="fa fa-angle-left pull-right"></i>
 							</span>
 						</a>
 						<ul class="treeview-menu">
-							<li><a href="reporte.php?accion=1"><i class="fa fa-circle-o"></i> Reserva por dia</a></li>
-							<li><a href="reporte.php?accion=2"><i class="fa fa-circle-o"></i> Entrada por dia</a></li>
+							<li><a href="reporte.php?accion=1"><i class="fa fa-check-circle-o"></i> Reserva por dia</a></li>
+							<li><a href="reporte.php?accion=2"><i class="fa fa-check-circle-o"></i> Entrada por dia</a></li>
 						</ul>
 					</li>
 				<?php } ?>
@@ -73,20 +93,16 @@
 				if (in_array('mantenimiento', $_SESSION['modulos'])) { ?>
 					<li class="treeview">
 						<a href="#">
-							<i class="fa fa-print"></i><span>Mantenimiento</span>
+							<i class="fa fa-wrench"></i><span>Mantenimiento o Servicios</span>
 							<span class="pull-right-container">
 								<i class="fa fa-angle-left pull-right"></i>
 							</span>
 						</a>
 						<ul class="treeview-menu">
-							<li><a href="tipo_documento.php"><i class="fa fa-circle-o"></i> Tipo documento</a></li>
-							<li><a href="producto.php"><i class="fa fa-circle-o"></i> Producto</a></li>
-							<li><a href="categorias.php"><i class="fa fa-circle-o"></i> Categorias</a></li>
+							<li><a href="producto.php"><i class="fa fa-check-circle-o"></i> Producto</a></li>
+							<li><a href="categorias.php"><i class="fa fa-check-circle-o"></i> Categorias</a></li>
 							
-							<li><a href="ciudad.php"><i class="fa fa-circle-o"></i> Ciudad</a></li>
-							<li><a href="pais.php"><i class="fa fa-circle-o"></i> Pais</a></li>
-							<li><a href="tipo_cliente.php"><i class="fa fa-circle-o"></i> Tipo cliente</a></li>
-							<li><a href="tipo_servicio.php"><i class="fa fa-circle-o"></i> Tipo servicio</a></li>
+							<li><a href="tipo_servicio.php"><i class="fa fa-check-circle-o"></i> Tipo servicio</a></li>
 						</ul>
 					</li>
 				<?php } ?>
@@ -94,16 +110,16 @@
 				if (in_array('usuarios', $_SESSION['modulos'])) { ?>
 					<li class="treeview">
 						<a href="#">
-							<i class="fa fa-print"></i> <span>Usuarios</span>
+							<i class="fa fa-user"></i> <span>Usuarios</span>
 							<span class="pull-right-container">
 								<i class="fa fa-angle-left pull-right"></i>
 							</span>
 						</a>
 						<ul class="treeview-menu">
-							<li><a href="empleados.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
-							<!--<li><a href="modulos.php"><i class="fa fa-circle-o"></i> Modulos</a></li>-->
-							<li><a href="tipo_empleado.php"><i class="fa fa-circle-o"></i> Tipo usuario</a></li>
-							<!--li><a href="permisos.php"><i class="fa fa-circle-o"></i> Permisos</a></li-->
+							<li><a href="empleados.php"><i class="fa fa-check-circle-o"></i> Usuarios</a></li>
+							<!--<li><a href="modulos.php"><i class="fa fa-check-circle-o"></i> Modulos</a></li>-->
+							<li><a href="tipo_empleado.php"><i class="fa fa-check-circle-o"></i> Tipo usuario</a></li>
+							<!--li><a href="permisos.php"><i class="fa fa-check-circle-o"></i> Permisos</a></li-->
 						</ul>
 					</li>
 				<?php
@@ -111,7 +127,7 @@
 				?>
 	        	
 	        	<li class="header">OTRAS OPCIONES</li>
-	        	<li><a href="#../index.php"><i class="fa fa-circle-o text-red"></i> 
+	        	<li><a href="#../index.php"><i class="fa fa-check-circle-o text-red"></i> 
 	        		<span>CERRAR SESION</span></a>
 	        	</li>
 	     	</ul>
